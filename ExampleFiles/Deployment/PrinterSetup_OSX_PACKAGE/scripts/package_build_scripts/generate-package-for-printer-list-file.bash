d2a1874859dbd281c661fbab21418e3cbf73c6a8
Henri Shustak <henri.shustak@gmail.com>
2011-05-12 13:05:38 +1200

    Adds some missing files from the previous commit.

---
+++ ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts/generate-package-for-printer-list-file.bash
@@ -0,0 +1,248 @@
+#!/bin/bash
+#
+# GNU GPL Copyright Henri Shustak 2011
+# 
+# Lucid Information Systems
+# http://www.lucidsystems.org
+#
+# Generates an apple package installer for the printer list file which is passed in as argument number one.
+# The apple package is generated within the directory passed in as argument number two.
+#
+# This script is based of the generate-package-for-printer-setup-file.bash within this direcotry.
+#
+# This script will require PrinterSetup PrinterSetup_v0041 or later.
+#
+#
+# Version 1.0
+# 
+# Version History 
+#   - 1.0 : initial release
+
+# Notes : Perhaps a using an option flag is a better approach for enabling overwriting of packages?
+
+# Configuration
+
+# Leave this set to no, unless you want to overwrite old packages (YES/NO)
+default_overwrite_existing_packages="NO"
+
+# Leave this set to yes, unless you want no report that a package was skipped (YES/NO)
+default_report_skipped_packages="YES"
+
+# Leave this set to no, unless you want to report that a package was overwritten (YES/NO)
+default_display_warning_on_package_overwriting_for_each_pacakge="NO"
+
+# Gather input arguments
+path_to_this_script="${0}"
+input_printer_list_file="${1}"
+output_directory="${2}"
+num_arguments=$#
+
+# internal varibles
+num_packages_skipped=0
+packages_created=0
+printer_setup_files_processed=0
+package_creation_errors=0
+parent_folder="`dirname \"${path_to_this_script}\"`"
+path_from_root="/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts"
+printer_setup_root="`echo \"${parent_folder}\"| awk -F \"${path_from_root}\" '{ print $1 }'`"
+room_number="OSXPACKAGE"
+buildscript_create_package="${printer_setup_root}/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts/printer_setup_build.bash"
+buildscript_default_package="${printer_setup_root}/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts/set_default_printer_system_build.bash"
+package_maker_document_create_pacakge="${printer_setup_root}/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/PrinterSetup.pmdoc"
+package_maker_document_default_pacakge="${printer_setup_root}/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/set_default_printer/set_default_printer.pmdoc"
+PrinterSetupLinks_Path="${printer_setup_root}/PrinterSetupLinks"
+setupPrinter_link_name="PLF-${room_number}-PrinterDroplet4Package-ForEachPLF" # note that this may need to be changed or perhaps a droplet created?
+setupPrinter="${PrinterSetupLinks_Path}/${setupPrinter_link_name}"
+printer_setup_script="${printer_setup_root}/PrinterSetup.sh"
+printersetup_root_path_detction_string_for_printer_setup_files="/PrinterSetupFiles/"
+printersetup_printer_setup_files_realiitive_link_path="../PrinterSetupFiles/"
+
+# Function(s)
+
+function remove_tmporary_link {
+	# Remove the temporarily created link created by this script (or the one which may exist prior to this script being run)
+	if [ -h "${setupPrinter}" ] ; then 
+		rm "${setupPrinter}"
+		if [ $? != 0 ] ; then
+			echo "    ERROR! Removing the temporary link created for package creation."
+			exit -1
+		fi
+	fi
+}
+
+
+function check_if_output_pakage_already_exists_within_output_directory {
+
+	# Check if package already exists within the output directory
+	if [ -e "${output_package}" ] ; then
+		if [ "${overwrite_existing_packages}" == "NO" ] ; then
+			# The existing package will not be overwritten
+			if [ "${report_skipped_packages}" == "YES" ] ; then
+				echo "    WARNING : Install package already exists in output directory."
+					echo "    Package creation for \"${plf_name}.pkg\" has been skipped."
+			fi
+			exit 2
+		else
+			# Remove the existing package from the output directory
+			sleep 1
+			rm -rf "${output_package}"
+			if [ $? != 0 ] ; then
+				echo "     ERROR! : Removing existing package : \"${output_package}\""
+				echo "              Package generation for this PLF has been skipped."
+				exit -1
+			fi
+		fi
+	fi
+
+}
+
+
+# Validation
+# If this is not overridden then leave it alone.
+if [ "${overwrite_existing_packages}" == "" ] ; then
+    # validate the current setting 
+    overwrite_existing_packages="${default_overwrite_existing_packages}"
+fi
+
+# Validate overwrite_existing_packages variable
+if [ "${overwrite_existing_packages}" != "YES" ] && [ "${overwrite_existing_packages}" != "NO" ] ; then
+    echo "     ERROR! : The overwrite_existing_packages variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+
+# If this is not overridden then leave it alone.
+if [ "${report_skipped_packages}" == "" ] ; then
+    # validate the current setting 
+    report_skipped_packages="${default_report_skipped_packages}"
+fi
+
+# Validate report_skipped_packages variable
+if [ "${report_skipped_packages}" != "YES" ] && [ "${report_skipped_packages}" != "NO" ] ; then
+    echo "     ERROR! : The report_skipped_packages variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+
+# If this is not overridden then leave it alone.
+if [ "${display_warning_on_package_overwriting_for_each_pacakge}" == "" ] ; then
+    # validate the current setting 
+    display_warning_on_package_overwirting_for_each_pacakge="${default_display_warning_on_package_overwriting_for_each_pacakge}"
+fi
+
+# Validate report_skipped_packages variable
+if [ "${display_warning_on_package_overwriting_for_each_pacakge}" != "YES" ] && [ "${display_warning_on_package_overwriting_for_each_pacakge}" != "NO" ] ; then
+    echo "     ERROR! : The display_warning_on_package_overwriting_for_each_pacakge variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+
+# Report that files will be overwritten
+if [ "${overwrite_existing_packages}" == "YES" ] && [ "${display_warning_on_package_overwirting_for_each_pacakge}" == "YES" ]; then
+    echo "     NOTE! : Overwriting of existing packages found within the output directory is enabled."
+    sleep 3
+fi
+
+
+# Checking the arguments
+if [ $num_arguments -ne 2 ] ; then
+        echo "    Usage : $0 <path_to_printer_setup_file> <output_directory_for_package>" #  [path_to_printer_setup_file]
+        exit -1
+fi
+
+# Checking the input file
+if ! [ -e "${input_printer_list_file}" ] ; then 
+	echo "ERROR! : Unable to locate input printer-list-file : $input_printer_list_file"
+	exit -1
+fi
+
+# Checking the output directory
+if ! [ -d "${output_directory}" ] ; then 
+	echo "ERROR! : Unable to locate output directory : $output_directory"
+	exit -1
+fi
+
+
+# Check the printer_setup_root directory exists.
+if ! [ -d "${printer_setup_root}" ] ; then 
+	echo "    ERROR! : Unable to locate the PrinterSetup root directory at the following path :"
+	echo "             ${printer_setup_root}"
+       exit -1
+fi
+
+# correct any trailing slashes in the output directory
+output_directory="`dirname \"${output_directory}\"`/`basename \"${output_directory}\"`"
+
+# Do something useful like make a printer queue setup package. 
+
+package_maker_document="${package_maker_document_create_pacakge}"
+buildscript="${buildscript_create_package}"
+
+plf_name="`basename \"${input_printer_list_file}\"`"
+output_package="${output_directory}/${plf_name}.pkg"
+
+check_if_output_pakage_already_exists_within_output_directory
+
+# Delete any link which may already exit. The sync commands may improve reliability
+# Note : There is a function specified below which this could be migrated to at some point in the future
+sync
+sleep 1
+rm -f "${setupPrinter}"
+if [ $? != 0 ] ; then
+	echo "    ERROR!: Unable to remove the existing PLF package link :"
+	echo "            ${setupPrinter}"
+	exit -1
+fi
+sync
+
+
+# Calculate the relative link to the printer setup file
+printer_setup_realitive_link_from_printersetupfiles_directory="`echo \"${input_printer_list_file}\" | awk -F \"${printersetup_root_path_detction_string_for_printer_setup_files}\" '{print $2 }'`"
+printer_setup_realitive_link="${printersetup_printer_setup_files_realiitive_link_path}${printer_setup_realitive_link_from_printersetupfiles_directory}"
+
+
+# Prepare to create the relative link to the printer setup file
+cd "${PrinterSetupLinks_Path}"
+if [ $? != 0 ] ; then
+	echo "    ERROR!: Preparing for creation of the PLF package link."
+	exit 2
+fi
+
+# Create the relative link to the printer setup file
+ln -s "${printer_setup_realitive_link}" "./${setupPrinter_link_name}"
+if [ $? != 0 ] ; then
+	echo "    ERROR!: Unable to create the relative PLF package link."
+	exit -1
+fi
+
+
+# Create the package
+
+"${buildscript}" "${package_maker_document}" "${output_package}"
+
+
+if [ $? != 0 ] ; then
+	remove_tmporary_link
+	echo "    ERROR! Building Package. Package building canceled."
+	exit -1
+else
+	remove_tmporary_link
+fi
+
+exit 0 
+ 
+
+
+
+
+
+
---
+++ ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts/generate-packages-for-printer-list-files-within-a-directory.bash
@@ -0,0 +1,290 @@
+#!/bin/bash
+
+# GNU GPL Copyright Henri Shustak 2011
+# 
+# Lucid Information Systems
+# http://www.lucidsystems.org
+#
+# Given an input directory provided as argument number 1 containing only PLF's for processing 
+# into the output directory provided as argument number 2 as install packages.
+#
+# This script is based upon generate-packages-for-printer-setup-files-within-a-directory.bash within the same directory.
+#
+# This script will require PrinterSetup PrinterSetup_v0054 or later.
+#
+#
+# Version 1.0
+# 
+# Version History 
+#   - 1.0 : initial release.
+
+# Configuration 
+
+# Leave this set to no, unless you want to overwrite old packages (YES/NO)
+default_overwrite_existing_packages="NO"
+
+# Leave this unless you would like to see errors for skipped packages (YES/NO)
+default_report_skipped_packages="NO"
+
+# Leave this unless you would like to be able to have .pkg files within your input directory (YES/NO)
+default_allow_pkg_files_within_input_directory="NO"
+
+# Leave this alone unless you would like a warning each time a package is overwritten 
+default_display_warning_on_package_overwriting_for_each_pacakge="NO"
+
+# Leave this alone unless you would not like a warning when you are overwritting packages
+default_display_warning_on_package_overwirting_for_processing_of_plfs_within_directory="YES"
+
+
+# Note : There are various reasons for not using 'shift' in order to process multiple files passed in from the command line.
+
+
+# Gather input arguments
+path_to_this_script="${0}"
+input_plf_dir="${1}"
+output_dir="${2}"
+num_arguments=$#
+
+
+# internal varibles
+current_plf=""
+current_plf_name=""
+exit_value=0
+packages_successfully_generated=0
+pacakges_skipped=0
+input_files_processed=0
+processing_errors=0
+parent_folder="`dirname \"${path_to_this_script}\"`"
+path_from_root="/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts"
+printer_setup_root="`echo \"${parent_folder}\"| awk -F \"${path_from_root}\" '{ print $1 }'`"
+path_to_build_script="${printer_setup_root}/ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/scripts/package_build_scripts/generate-package-for-printer-list-file.bash"
+
+# functions(s)
+
+function make_package_for_current_plf {
+	if [ "${build_default_printer_installers_not_printer_creation_installers}" == "NO" ] ; then
+		echo -n "building install package for ${current_plf_name}..."
+	else
+		echo -n "building default package for ${current_plf_name}..."
+	fi
+	"${path_to_build_script}" "${current_plf}" "${output_dir}"
+	build_pacakge_return=$?
+	if [ ${build_pacakge_return} != 0 ] ; then
+		if [ ${build_pacakge_return} == 2 ] ; then
+			# package already exits and we are not overwriting
+			echo "already exits - skipped package creation"
+			return 2
+		else
+			echo "failed"
+			return 1
+		fi
+	fi
+	echo "done"
+	return 0
+}
+
+
+# Checking the arguments
+if [ $num_arguments -ne 2 ] ; then
+        echo "    Usage : $0 <path_to_directory_only_containing_printer_setup_file> <output_directory_for_packages>"
+        exit -1
+fi
+
+# Checking the output directory
+if ! [ -d "${output_dir}" ] ; then 
+	echo "ERROR! : Unable to locate the specified output directory : $output_dir"
+	exit -1
+fi
+
+# Checking the input directory
+if ! [ -d "${input_plf_dir}" ] ; then 
+	echo "ERROR! : Unable to locate the specified input directory : $input_plf_dir"
+	exit -1
+fi
+
+# Check the printer_setup_root directory exists.
+if ! [ -d "${printer_setup_root}" ] ; then 
+	echo "    ERROR! : Unable to locate the PrinterSetup root directory at the following path :"
+	echo "             ${printer_setup_root}"
+       exit -1
+fi
+
+# correct any trailing slashes in the output directory
+output_dir="`dirname \"${output_dir}\"`/`basename \"${output_dir}\"`"
+
+
+# Validation
+# If this is not overridden then leave it alone.
+if [ "${overwrite_existing_packages}" == "" ] ; then
+    # validate the current setting 
+    overwrite_existing_packages="${default_overwrite_existing_packages}"
+fi
+export overwrite_existing_packages
+
+# Validate overwrite_existing_packages varible
+if [ "${overwrite_existing_packages}" != "YES" ] && [ "${overwrite_existing_packages}" != "NO" ] ; then
+    echo "     ERROR! : The overwrite_existing_packages variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+# If this is not overridden then leave it alone.
+if [ "${report_skipped_packages}" == "" ] ; then
+    # validate the current setting 
+    report_skipped_packages="${default_report_skipped_packages}"
+fi
+export report_skipped_packages
+
+# Validate report_skipped_packages variable
+if [ "${report_skipped_packages}" != "YES" ] && [ "${report_skipped_packages}" != "NO" ] ; then
+    echo "     ERROR! : The report_skipped_packages variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+# data input validation
+# If this is not overridden then leave it alone.
+if [ "${allow_pkg_files_within_input_directory}" == "" ] ; then
+    # validate the current setting 
+    allow_pkg_files_within_input_directory="${default_allow_pkg_files_within_input_directory}"
+fi
+
+# Validate allow_pkg_files_within_input_directory variable
+if [ "${allow_pkg_files_within_input_directory}" != "YES" ] && [ "${allow_pkg_files_within_input_directory}" != "NO" ] ; then
+    echo "     ERROR! : The allow_pkg_files_within_input_directory variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+# If this is not overridden then leave it alone.
+if [ "${display_warning_on_package_overwriting_for_each_pacakge}" == "" ] ; then
+    # validate the current setting 
+    display_warning_on_package_overwriting_for_each_pacakge="${default_display_warning_on_package_overwriting_for_each_pacakge}"
+fi
+export display_warning_on_package_overwriting_for_each_pacakge
+
+# Validate display_warning_on_package_overwriting_for_each_pacakge variable
+if [ "${display_warning_on_package_overwriting_for_each_pacakge}" != "YES" ] && [ "${display_warning_on_package_overwriting_for_each_pacakge}" != "NO" ] ; then
+    echo "     ERROR! : The display_warning_on_package_overwriting_for_each_pacakge variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+# If this is not overridden then leave it alone.
+if [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" == "" ] ; then
+    # validate the current setting 
+    display_warning_on_package_overwirting_for_processing_of_plfs_within_directory="${default_display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}"
+fi
+export display_warning_on_package_overwriting_for_each_pacakge
+
+# Validate display_warning_on_package_overwirting_for_processing_of_plfs_within_directory variable
+if [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" != "YES" ] && [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" != "NO" ] ; then
+    echo "     ERROR! : The display_warning_on_package_overwirting_for_processing_of_plfs_within_directory variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+# If this is not overridden then leave it alone.
+if [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" == "" ] ; then
+    # validate the current setting 
+    display_warning_on_package_overwirting_for_processing_of_plfs_within_directory="${default_display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}"
+fi
+export display_warning_on_package_overwriting_for_each_pacakge
+
+# Validate display_warning_on_package_overwirting_for_processing_of_plfs_within_directory variable
+if [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" != "YES" ] && [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" != "NO" ] ; then
+    echo "     ERROR! : The display_warning_on_package_overwirting_for_processing_of_plfs_within_directory variable is not valid. It must be set to \"YES\" or \"NO\"."
+    echo "              Please check your shell is clean or that this shell variable is exported as a valid option."
+    echo "              The env command will typically provide a list of environment variables"
+    echo "              The default option is \"NO\" ; as to not overwrite existing packages."
+    exit -1
+fi
+
+
+# Report that files will be overwritten
+if [ "${overwrite_existing_packages}" == "YES" ] && [ "${display_warning_on_package_overwirting_for_processing_of_plfs_within_directory}" == "YES" ]; then
+	echo ""
+    echo "     NOTE! : Overwriting of existing packages found within the output directory is enabled."
+	echo ""
+    sleep 3
+fi
+
+
+# check input directory for any .pkg files. If.pkg files are found then exit as this may not be the correct input directory.
+if [ "${allow_pkg_files_within_input_directory}" == "NO" ] ; then
+	number_of_packages_within_the_input_directory=`ls -l "${input_plf_dir}/"*.pkg 2>> /dev/null | wc -l | awk '{print $1}'`
+	if [ $number_of_packages_within_the_input_directory != 0 ] ; then
+		echo "    ERROR! : Package files (.pkg) were detected within the output directory."
+		echo "             Please check that the output directory is listed as the second argument"
+		echo "             and that there are no .pkg files within this directory."
+		exit -1
+	fi
+fi
+
+
+
+# process the input directory
+
+for item in "${input_plf_dir}/"*; do
+                
+		current_plf="$item"
+        current_plf_name=`basename "$current_plf"`	
+	
+        if [ -s "${current_plf}" ]; then
+
+        	((input_files_processed++))
+
+			#output_package_path="${output_dir}/${current_plf_name}.pkg"
+
+        	# process the plf
+        	make_package_for_current_plf
+                    
+		# Check that worked
+        	exit_value=$?
+        	if [ $exit_value == 0 ]; then
+			((packages_successfully_generated++))
+		else
+			if [ $exit_value != 2 ]; then
+				((processing_errors++))				
+			else
+				((pacakges_skipped ++))
+			fi
+		fi
+        fi        
+done
+
+# Print a summary 
+echo ""
+echo ""
+if [ "${build_default_printer_installers_not_printer_creation_installers}" == "NO" ] ; then
+	echo "Summary (install package creation) : "
+else
+	echo "Summary (default package creation) : "
+fi
+echo "======================================================== "
+echo "Number of processed input files                    : $input_files_processed"
+echo "Number of packages generated successfully          : $packages_successfully_generated"
+echo "Number of packages skilled                         : $pacakges_skipped"
+echo "Number of processing errors                        : $processing_errors"
+echo ""
+echo ""
+
+# return an error code (exit code) if any processing errors were detected
+if [ ${processing_errors} != 0 ] ; then
+	exit -1
+fi
+
+exit 0 
+
+
+
