da0af2dd359d38024c0a02bfa743a7490f69fd62
Henri Shustak <henri.shustak@gmail.com>
2012-02-02 13:52:41 +1300

    Updated the OS X package "set_default_printer.sh" to provide name-prefixing support when building default queue install packages.

--- ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/set_default_printer/set_default_printer.sh
+++ ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/set_default_printer/set_default_printer.sh
@@ -8,17 +8,17 @@
 # This is the script which is run when you install the related package.
 # It is designed to set the default printer.
 
-# Version 0.2
+# Version 0.3
 
 # Version history
-# 0.1  - testubg
+# 0.1  - test and debug
 # 0.2  - initial release
+# 0.3  - support for name prefixing when it is enabled in the PrinterSetupPackage.sh script.
 
 ## This is helpful for if you modify this script and need help debugging.
 #echo $0 >> /tmp.txt
 #ls -l >> /tmp.txt
 #echo "" >> /tmp.txt
-
 
 
 
@@ -43,6 +43,21 @@ export printer_prefixing="NO"
 
 # Change into the scripts root directory (PrinterSetup root).
 cd "${printer_setup_folder}"
+
+# Retrive the name_prefix (if any) from the "PrinterSetupPackage.sh" script and export this value
+path_to_printer_setup_package_script_from_root="./ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/PrinterSetupPackage.sh"
+path_to_printer_setup_script_from_root="./PrinterSetup.sh"
+export printer_name_prefix=`cat "${path_to_printer_setup_package_script_from_root}" | grep "package_queue_name_prefix=" | awk -F "package_queue_name_prefix=\"" '{print $2}' | awk -F "\"" '{print $1}'`
+export printer_name_prefix_delimiter=`cat "${path_to_printer_setup_script_from_root}" | grep "printer_name_prefix_delimiter=" | awk -F "printer_name_prefix_delimiter=\"" '{print $2}' | awk -F "\"" '{print $1}'`
+if [ "${printer_name_prefix}" != "" ] ; then
+	export printer_prefixing="YES"
+fi
+
+
+printer_description_prefix="${printer_description_prefix}"
+printer_description_prefix_delimiter="${printer_description_prefix_delimiter}"
+
+
 
 # Find the PSF which will be used to set to setup the default printer (realitive to PrinterSetup root).
 default_printer_setup_dir="ExampleFiles/Deployment/PrinterSetup_OSX_PACKAGE/set_default_printer/PSF_default_printer_directory"
